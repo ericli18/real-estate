c024b814b74e24acbc9d92cf28039e9367393c60
Eric Li <eric.li.8736@gmail.com>
2023-01-17 14:10:20 -0600

    Add Svelte, create room selection component

---
+++ form-component/src/App.svelte
@@ -0,0 +1,5 @@
+<script>
+  import Room from './Room.svelte';
+</script>
+
+<Room/>
---
+++ form-component/src/Room.svelte
@@ -0,0 +1,23 @@
+<script>
+    import '@picocss/pico'
+    import { each } from 'svelte/internal';
+    import './app.css'
+    const options = ['bedroom', 'bathroom', 'living room'
+    , 'yard', 'garage', 'kitchen'];
+  </script>
+  
+  <main>
+   <form>
+    <select>
+      <option value="" disabled selected>Select</option>
+      {#each options as option}
+        <option>{option}</option>
+      {/each}
+    </select>
+   </form>
+  </main>
+  
+  <style>
+  
+  </style>
+  
